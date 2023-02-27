2e7437057e99d364a033c980a4bc44d324d67bae
Tom Bourjala <tom.bourjala@gmail.com>
2023-02-27 15:29:57 +0100

    seperation of diseases and symptoms in seperated binary dataframes

--- main.py
+++ main.py
@@ -3,15 +3,35 @@ from pandas import read_csv
 
 df = read_csv('resources/dataset.csv')
 
-# count every iteration of a symptom (all columns are named from Symptom_1 to Symptom_17)
-count = dict()
+symptoms = []
 
 for i in range(1, 18):
     for symptom in df['Symptom_' + str(i)]:
-        if symptom in count:
-            count[symptom] += 1
-        else:
-            count[symptom] = 1
+        if symptom not in symptoms:
+            symptoms.append(symptom)
 
-for key, value in count.items():
-    print(key, value)
+diseases = []
+
+for disease in df['Disease']:
+    if disease not in diseases:
+        diseases.append(disease)
+
+df_diseases = pandas.DataFrame(columns=diseases)
+df_symptoms = pandas.DataFrame(columns=symptoms)
+
+for i in range(0, 20):
+    disease = df['Disease'][i]
+    rawSymptoms = []
+    for j in range(1, 18):
+        symptom = df['Symptom_' + str(j)][i]
+        # append if not nan
+        if not pandas.isnull(symptom):
+            rawSymptoms.append(symptom)
+    # append the raw data, putting the disease and symptom raw to 1 if it is present, else 0
+    df_diseases.loc[i] = [1 if disease == d else 0 for d in diseases]
+    df_symptoms.loc[i] = [1 if symptom in rawSymptoms else 0 for symptom in symptoms]
+
+print(df_diseases)
+print(df_symptoms)
+
+# %%
